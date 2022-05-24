f5f7023e17666ef632fab4b3ecafd9c4e904935d
Dang Trung Hieu <dangtrunghieu0207@gmail.com>
2022-05-24 15:37:24 +0700

    `CorePhpMVC` - Delete redundant core

--- app/translates/resources/common/common.vi.php
+++ app/translates/resources/common/common.vi.php
@@ -1,16 +1,6 @@
 <?php
 
 const COMMON_LANGUAGES = [
-    // Menu
-    'manage_user' => 'Quản lí users',
-    'manage_medical_files' => 'Quản lí hồ sơ bệnh nhân',
-    'manage_medicine' => 'Quản lí thuốc',
-    'manage_medicine_type' => 'Quản lí loại thuốc',
-    'manage_blog' => 'Quản lí bài viết',
-    'manage_contact' => 'Quản lí liên hệ',
-    'manage_health_declaration' => 'Quản lí khai báo y tế',
-    'calendar' => 'Lịch',
-
     // Button
     'delete' => 'Xóa',
     'cancel' => 'Hủy',
